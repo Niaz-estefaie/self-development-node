c860c19d190dbf0fd66db548be7c4fbdac6c45e5
Niaz <niaz.e@bugloos.com>
2024-10-06 17:54:53 +0330

    (feature): modules added to the project

---
+++ src/controllers/moduleController.js
@@ -0,0 +1,47 @@
+const Module = require('../models/moduleModel');
+
+exports.createModule = async (req, res) => {
+  try {
+    const { name, title, order, description } = req.body;
+    const newModule = await Module.create({ name, title, order, description });
+    res.status(201).json(newModule);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getModules = async (req, res) => {
+  try {
+    const modules = await Module.find().sort({ order: 1 });
+    res.status(200).json(modules);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.updateModuleOrder = async (req, res) => {
+  try {
+    const { moduleId, newOrder } = req.body;
+    const module = await Module.findById(moduleId);
+
+    if (!module) {
+      return res.status(404).json({ message: 'Module not found' });
+    }
+
+    module.order = newOrder;
+    await module.save();
+    res.status(200).json({ message: 'Order updated', module });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.deleteModule = async (req, res) => {
+  try {
+    const moduleId = req.params.id;
+    const deletedModule = await Module.findByIdAndDelete(moduleId);
+    res.status(200).json({ message: 'Module deleted', deletedModule });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
