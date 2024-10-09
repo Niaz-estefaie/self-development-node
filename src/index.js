55f68177e000ede5d1e9cef6ce482a2e90a919f3
Niaz <niaz.e@bugloos.com>
2024-10-09 16:49:20 +0330

    (fix): worked on managing a bug

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const moduleRoutes = require("./routes/moduleRoutes");
+
+const app = express();
 
 dotenv.config();
 
 connectDB();
 
-const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 
 const BASE_URL = "/api/v1";
-app.use(`${BASE_URL}`, authRoutes)
-  .use(`${BASE_URL}`, moduleRoutes)
-  .use(`${BASE_URL}`, userRoutes);
+app.use(`${BASE_URL}/auth`, authRoutes);
+app.use(`${BASE_URL}/modules`, moduleRoutes);
+app.use(`${BASE_URL}/users`, userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
